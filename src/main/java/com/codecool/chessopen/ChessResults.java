7139a430f4a65caf8b8b981292b427824f86503c
mjoe92 <mcsurgai@gmail.com>
2021-03-24 15:46:17 +0100

    Done!

--- src/main/java/com/codecool/chessopen/ChessResults.java
+++ src/main/java/com/codecool/chessopen/ChessResults.java
@@ -1,20 +1,44 @@
 package com.codecool.chessopen;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.io.*;
+import java.util.*;
 
 public class ChessResults {
 
     public List<String> getCompetitorsNamesFromFile(String fileName){
-        //TODO your code comes here
-        return null;
+        File file = new File(fileName);
+        List<String> lines = new ArrayList<>();
+        List<String> results = new ArrayList<>();
+        Map<String, Integer> calculatedResults = new HashMap<>();
+        try {
+            Scanner reader = new Scanner(file, "UTF-8");
+            while (reader.hasNextLine()) {
+                lines.add(reader.nextLine());
+            }
+            reader.close();
+            for (String line : lines) {
+                List<String> all = Arrays.asList(line.split(","));
+                int sum = 0;
+                for (int i = 1; i <= 5; i++) {
+                    sum += Integer.parseInt(all.get(i));
+                }
+                calculatedResults.put(all.get(0), sum);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found!");
+        }
+        List<Integer> sortedList = new ArrayList<>(calculatedResults.values());
+        sortedList.sort(Collections.reverseOrder());
+        for (Integer result : sortedList) {
+            for (String name : calculatedResults.keySet()) {
+                if (calculatedResults.get(name).equals(result)) {
+                    results.add(name);
+                    calculatedResults.remove(name);
+                    break;
+                }
+            }
+        }
+        return results;
     }
 
 }
